f88a97b5f89aed5cbff0fe4c4971233e9c3b8fac
unknown <rivas.fujos20@triana.salesianos.edu>
2021-10-27 23:00:44 +0200

    He añadido el hola mundo

--- HolaMundo/src/holamundo/Principal.java
+++ HolaMundo/src/holamundo/Principal.java
@@ -1,18 +1,12 @@
 package holamundo;
 
-
-
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
+		System.out.println("HolaMundo");
 	
-		System.out.println("Hola Mundo");
-		
-		
-		
-		
 	}
 
 }
