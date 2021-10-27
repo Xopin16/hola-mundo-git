1a6c344a2760248d7cd963d17bcb237191a12053
unknown <rivas.fujos20@triana.salesianos.edu>
2021-10-27 22:44:53 +0200

    He añadido el proyecto java HolaMundo

---
+++ HolaMundo/src/holamundo/Principal.java
@@ -0,0 +1,18 @@
+package holamundo;
+
+
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	
+		System.out.println("Hola Mundo");
+		
+		
+		
+		
+	}
+
+}
